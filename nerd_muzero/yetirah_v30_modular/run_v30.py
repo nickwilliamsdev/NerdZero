c1d49ea5c6c061dd21395986bbcd005ae889ff25
nickwilliamsdev <nickwilliamsdevstuff112358@proton.me>
2026-09-14 01:10:43 +0000

    attempting to nerd_muzero

--- nerd_muzero/yetirah_v30_modular/run_v30.py
+++
@@ -1,4 +0,0 @@
-from sefer.experiments.synthetic_v30 import run
-
-if __name__ == "__main__":
-    run()
--- nerd_muzero/yetirah_v30_modular/sefer/__init__.py
+++
@@ -1,5 +0,0 @@
-from sefer.config import V30Config
-from sefer.controllers.reasoner import TinyReasoner
-from sefer.tasks.synthetic_algebra import SyntheticTaskBatch
-
-__all__ = ["V30Config", "TinyReasoner", "SyntheticTaskBatch"]
